8265eb844b3c1d5f11464dff13259b4a47828e90
gracious <atigborgracious@gmail.com>
2018-10-23 02:02:49 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,13 +13,13 @@ function parallax(){
     
      $('.parallax--box3').css('top',-10+(wScroll*0.008)+'em')   
     
-    $('.parallax--box4').css('top',-20+(wScroll*0.009)+'em')
+    $('.parallax--box4').css('top',-22+(wScroll*0.010)+'em')
      
     $('.parallax--box5').css('top',-16+(wScroll*0.007)+'em')   
     
      $('.parallax--box6').css('top',-18+(wScroll*0.008)+'em')
     
-     $('.parallax--box7').css('top',-33+(wScroll*0.015)+'em')
+     $('.parallax--box7').css('top',-31+(wScroll*0.015)+'em')
     
      $('.parallax--box8').css('top',-25+(wScroll*0.009)+'em')
      
